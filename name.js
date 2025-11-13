ed50f4c7352f85e74db1cc7f9843b2a095681f21
Pradeep <bpradeep080504@gmail.com>
2025-11-13 11:39:58 +0530

    commet

---
+++ name.js
@@ -0,0 +1,28 @@
+// let text1="pradeep";
+// console.log(text1.length);
+// console.log(text1.charAt(2));
+
+//let text2 = "kiran";
+//let text1 = "pradeep";
+//let value = text1.concat(" ", text2);
+//console.log(value);
+// let text = "   hello   ";
+// console.log(text.toLocaleUpperCase());
+// console.log(text.toLocaleLowerCase());
+// console.log(text.repeat(2));
+// console.log(text.trim());
+// console.log(text.trimEnd());
+
+//--possition value enter eill get negative value
+//let text=" I am in Banglore";
+//console.log(text.search("g"));
+//console.log(text.split("").reverse().join(""));
+//console.log(text.split("").join(""));
+
+let array = [1, 2, 3, 4, 5];
+let data = new Array();
+console.log(array);
+console.log(data);
+array.push(6);
+console.log(array);
+array.pop();
---
+++ test1.js
@@ -0,0 +1,31 @@
+// // let type = "tier2";
+// // switch(type){
+// //     case "tier1":
+// //         console.log("i have a pakage of 10 lpa");
+// //         break;
+
+// // case "tier2":
+// //     console.log("i have a pakage of 15lpa");
+// //     break;
+
+// // case "tier3":
+// //     console.log("i have a pakage of 20lap");
+// //     break;
+// //     default:
+// //         console.log("own business");
+
+// // }
+// function pake(type) {
+//   if (type == "test1") {
+//     console.log("5lpa");
+//   } else if (type == "test2") {
+//     console.log("7lpa");
+//   } else if (type == "test3") {
+//     console.log("10lpa");
+//   } else {
+//     console.log("own business");
+//   }
+// }
+// pake("test1");
+// pake("test2");
+// pake("other");
